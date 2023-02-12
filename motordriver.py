3e73170af0f75f1ddc1663c9fc3ae0d9c3a2654a
V16nesh <v16nesh2003@gmail.com>
2023-02-12 16:00:35 +0530

    Commented

--- motordriver.py
+++ motordriver.py
@@ -1,16 +1,22 @@
+#AtomRobotics
+#Raxous-V2
 from machine import Pin
 
+#setting up pwm pins
 pwml = PWM(Pin(21))
 pwmr = PWM(pin(22))
+
+#setting up h bridge oins
 lin1 = Pin(26, Pin.OUT)
 lin2 = Pin(27, Pin.OUT)
 rin1 = Pin(24, Pin.OUT)
 rin2 = Pin(25, Pin.OUT)
 
+#setting frequency
 pwml.freq(1000)
 pwmr.freq(1000)
 
-
+#we are giving duty as 2^10 bit value so it can accept values from 0 to 1023 
 def movement(lspeed, rspeed):
     if lspeed < 0:
         lspeed = -lspeed
